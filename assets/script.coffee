c62cb8aeab8921fd52df0191c5ef6a14d43629fe
fabianelima <fabianelim@gmail.com>
2016-07-27 10:01:17 -0300

    Mais uma tentativa
    Ainda não deu certo e também estou comitando esse pra deixar o código
    guardado de alguma forma, mas pelo menos já entendi melhor como tenho
    que fazer.

--- assets/script.coffee
+++ assets/script.coffee
@@ -1,4 +1,33 @@
 $ ->
-	$('#container').on 'click', ->
-		localStorage.setItem('item', 'teste')
-		$('.blob').html(localStorage.getItem('item'))
+	i = 0;
+
+	teste = {
+		teste1: 'testetes testes',
+		teste2: 'lçdkgjçidjlk'
+	}
+
+	$('.add-list').on 'click', ->
+		$(this).hide()
+		$('.new-list').show()
+
+	$('.new-list .ok').on 'click', ->
+		if $('.add-title').val() == '' or $('.add-item').val() == ''
+			$('.add-item,.add-title').css('background','red')
+		else
+			i++
+			localStorage.setItem('list', $('.add-title').val())
+			localStorage.setItem('item' + i, $('.add-item').val())
+			$('.list h3').html(localStorage.getItem('list'))
+			$('.list ul').append('<li>' + localStorage.getItem('item' + i) + '</li>')
+			$('.add-title').hide()
+			$('.add-item').removeAttr('required')
+			$('.add-item').val('')
+			$('.list').show()
+			# teste
+			localStorage.setItem('teste', teste.teste1)
+			console.log(localStorage.getItem('teste'))
+
+	$('.sv').on 'click', ->
+		$('.lists').show()
+		$('.lists').append('<div class="list' + i + '">' + $('.list').html() + '</div>') # talvez salvar isso num json funcione
+		$('.list').html('')
--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,36 @@
 (function() {
   $(function() {
-    return $('#container').on('click', function() {
-      localStorage.setItem('item', 'teste');
-      return $('.blob').html(localStorage.getItem('item'));
+    var i, teste;
+    i = 0;
+    teste = {
+      teste1: 'testetes testes',
+      teste2: 'lçdkgjçidjlk'
+    };
+    $('.add-list').on('click', function() {
+      $(this).hide();
+      return $('.new-list').show();
+    });
+    $('.new-list .ok').on('click', function() {
+      if ($('.add-title').val() === '' || $('.add-item').val() === '') {
+        return $('.add-item,.add-title').css('background', 'red');
+      } else {
+        i++;
+        localStorage.setItem('list', $('.add-title').val());
+        localStorage.setItem('item' + i, $('.add-item').val());
+        $('.list h3').html(localStorage.getItem('list'));
+        $('.list ul').append('<li>' + localStorage.getItem('item' + i) + '</li>');
+        $('.add-title').hide();
+        $('.add-item').removeAttr('required');
+        $('.add-item').val('');
+        $('.list').show();
+        localStorage.setItem('teste', teste.teste1);
+        return console.log(localStorage.getItem('teste'));
+      }
+    });
+    return $('.sv').on('click', function() {
+      $('.lists').show();
+      $('.lists').append('<div class="list' + i + '">' + $('.list').html() + '</div>');
+      return $('.list').html('');
     });
   });
 
